cce79a6264114f93cdb180b1c73d66f4d40dbf3a
creat899 <yofilo7713@aosod.com>
2023-03-21 19:30:21 +0500

    Добавлен метод чтения записей

--- WFDBReader.c
+++ WFDBReader.c
@@ -1,1 +1,41 @@
 #include "WFDBReader.h"
+
+#include <stdio.h>
+#include <malloc.h>
+#include <wfdb/wfdb.h>
+
+//вывести входные сигналы
+int outSignal(char *name) {
+	int i, j, nsig;
+	WFDB_Sample *v;
+	WFDB_Siginfo *s;
+	
+	//открытие файла сигналов
+	nsig = isigopen(name, NULL, 0);
+	
+	if (nsig < 1) {
+		//выход так как ошибка открытия
+		return 1;
+	}
+	
+	s = (WFDB_Siginfo *)malloc(nsig * sizeof(WFDB_Siginfo));
+	
+	if (isigopen(name, s, nsig) != nsig) {
+		return 1;
+	}
+	
+	v = (WFDB_Sample *)malloc(nsig * sizeof(WFDB_Sample));
+	
+	while (getvec(v) >= 0) {
+    	for (j = 0; j < nsig; j++) {
+    		printf("%8d", v[j]);
+		}
+		
+		printf("\n");
+	}
+	
+	free(v);
+	free(s);
+	
+	return 0;
+}
--- WFDBReader.h
+++ WFDBReader.h
@@ -1,6 +1,11 @@
+/*-------------------------------------------------------
+ gcc -c WFDBReader.c -o WFDBReader.o -I/usr/local/include
+-------------------------------------------------------*/
+
 #ifndef WFDBREADER_H
 #define WFDBREADER_H
 
-
+//вывести входные сигналы
+int outSignal(char *name);
 
 #endif /*WFDBREADER_H*/
