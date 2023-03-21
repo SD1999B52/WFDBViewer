dfc98aedd449077693a5c2e4756b0eec2306af81
creat899 <yofilo7713@aosod.com>
2023-03-21 19:42:52 +0500

    Чтение аннотации

--- WFDBReader.c
+++ WFDBReader.c
@@ -39,3 +39,29 @@ int outSignal(char *name) {
 	
 	return 0;
 }
+
+//вывести аннотацию
+int outAnnotation(char *name) {
+	WFDB_Anninfo a;
+	WFDB_Annotation annot;
+	
+	//количество WFDB_Anninfo структур в массиве
+	a.name = "atr";
+	a.stat = WFDB_READ;
+	
+	//1 - это количество WFDB_Anninfo структур
+	//WFDB_Anninfo может быть массив
+	if (annopen(name, &a, 1) < 0) {
+		//выход так как ошибка открытия
+		return 1;
+	}
+	
+	while (getann(0, &annot) == 0) {
+		char *time = mstimstr(annot.time);
+		char *code = annstr(annot.anntyp);
+		
+		printf("%s %s\n", time, code);
+	}
+	
+	return 0;
+}
