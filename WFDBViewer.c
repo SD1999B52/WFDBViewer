154be86aac5e82752fbbf589fe97e9cf6f121f7f
SD1999B52 <d.shalupin@mail.ru>
2023-03-21 19:57:29 +0500

    Добавление включений

--- WFDBViewer.c
+++ WFDBViewer.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "WFDBReader.h"
+
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+
+#include <gtkdatabox.h>
+#include <gtkdatabox_points.h>
+#include <gtkdatabox_ruler.h>
 
 int main(int argc, char *argv[]) {
     
